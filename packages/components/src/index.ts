d1e13bcf2562a4a4fc39358fe889505d7c0914f6
Beae-TuanNguyen <139732634+Beae-TuanNguyen@users.noreply.github.com>
2023-07-25 17:29:38 +0700

    BSPB-87: Make text component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,8 @@
 export * from "./button"
+<<<<<<< HEAD
 export * from "./grid"
 export * from "./checkbox"
 export * from "./choice"
+=======
+export * from "./text"
+>>>>>>> a6df849 (BSPB-87: Make text component)
---
+++ packages/components/src/text/index.ts
@@ -0,0 +1,2 @@
+export { Text } from "./text"
+export { useText } from "./use-text"
