b85ce30ac6860d0ec5ebf81c3ab00caa11ad8b3b
Beae-TuanNguyen <139732634+Beae-TuanNguyen@users.noreply.github.com>
2023-07-27 14:48:53 +0700

    BSPB-98: Make beae-ui code component

---
+++ packages/components/code/stories/code.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/vue3"
+
+import { Code } from "../src"
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+const meta = {
+  title: "Example/Code",
+  component: Code as any,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {
+    size: { control: "select", options: ["xs", "sm", "md", "lg"] },
+    variant: {
+      control: "select",
+      options: ["ghost", "outline", "solid", "link", "unstyled"],
+    },
+  },
+  args: {
+    size: "md",
+    variant: "solid",
+  },
+} satisfies Meta<typeof Code>
+
+export default meta
+type Story = StoryObj<typeof meta>
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/vue/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: (args) => ({
+    components: { Code },
+    setup() {
+      return {
+        args,
+      }
+    },
+    template: `<Code>Example Code</Code>`,
+  }),
+  args: {},
+}
