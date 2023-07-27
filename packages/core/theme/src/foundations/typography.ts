07d7271c4cb83b6f8fdf3acebf5f2867b9ee6f4b
ducanbeae <dunguyen@beae.com>
2023-07-27 16:21:25 +0700

    BSPB-120: update font experimental shopify

--- packages/core/theme/src/foundations/typography.ts
+++ packages/core/theme/src/foundations/typography.ts
@@ -39,9 +39,9 @@ const typography = {
   },
 
   fonts: {
-    heading: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-    body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-    mono: `SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
+    heading: `-apple-system,Inter, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    body: `-apple-system,Inter, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    mono: `SFMono-Regular,Inter,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
   },
 
   fontSizes: {
