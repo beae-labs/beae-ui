ed8fc90bcf6a6838dc3ee506140970272b5f091b
ducanbeae <dunguyen@beae.com>
2023-07-28 21:31:48 +0700

    BSPB-104: update ui skeleton

---
+++ packages/components/skeleton/src/skeleton-circle.ts
@@ -0,0 +1,28 @@
+import { h, ref, defineComponent, toRefs } from "vue"
+import {
+  ComponentWithProps,
+  beae,
+  DeepPartial,
+  ThemingProps,
+} from "@beae-ui/system"
+import { vueThemingProps } from "@beae-ui/prop-utils"
+
+import { Skeleton } from "./skeleton"
+
+export const SkeletonCircle: ComponentWithProps<DeepPartial<ThemingProps>> =
+  defineComponent({
+    props: {
+      ...vueThemingProps,
+    },
+    setup(props) {
+      console.log("props: ", props)
+
+      const { size = ref("2rem") } = toRefs(props)
+      return () => {
+        return h(beae(Skeleton, { label: "skeleton__circle" }), {
+          borderRadius: "full",
+          boxSize: size.value,
+        })
+      }
+    },
+  })
---
+++ packages/components/skeleton/stories/skeleton.stories.ts
@@ -0,0 +1,114 @@
+import type { StoryFn } from "@storybook/vue3"
+import { SkeletonCircle, SkeletonText, Skeleton } from "../src"
+import { Button } from "../../button"
+import { Box, Stack } from "../../layout"
+import { ref } from "vue"
+
+export default {
+  title: "Components / Skeleton",
+  component: { SkeletonCircle, SkeletonText, Skeleton, Box },
+}
+
+const BasicTemplate: StoryFn = (args: any) => ({
+  components: { SkeletonCircle, SkeletonText, Skeleton, Stack, Box },
+  setup() {
+    return { args }
+  },
+  template: `
+    <h2> Basic </h2>
+    <Stack>
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+    </Stack>
+    <Skeleton>
+      <div>contents wrapped</div>
+      <div>won't be visible</div>
+    </Skeleton>
+    `,
+})
+
+export const Basic = BasicTemplate.bind({})
+
+const SkeletonColorTemplate: StoryFn = (args: any) => ({
+  components: { SkeletonCircle, SkeletonText, Skeleton, Stack, Box },
+  setup() {
+    return { args }
+  },
+  template: `
+    <h2> Skeleton color# </h2>
+    <Skeleton startColor='pink.500' endColor='orange.500' height='50px' />
+    `,
+})
+
+export const SkeletonColor = SkeletonColorTemplate.bind({})
+
+const CircleTemplate: StoryFn = (args: any) => ({
+  components: { SkeletonCircle, SkeletonText, Skeleton, Stack, Box },
+  setup() {
+    return { args }
+  },
+  template: `
+    <h2> Circle and Text Skeleton#
+    </h2>
+    <Box padding='6' boxShadow='lg' bg='white'>
+      <SkeletonCircle size='10' />
+      <SkeletonText :mt='4' :noOfLines="4" spacing='4' skeletonHeight='2' />
+    </Box>
+    `,
+})
+
+export const CircleAnText = CircleTemplate.bind({})
+
+const ToggleTemplate: StoryFn = (args: any) => ({
+  components: {
+    SkeletonCircle,
+    SkeletonText,
+    Skeleton,
+    Box,
+    Button,
+    Stack,
+  },
+  setup() {
+    const isLoaded: any = ref(false)
+
+    function setIsLoaded() {
+      isLoaded.value = !isLoaded.value
+      console.log("isLoaded", isLoaded.value)
+    }
+
+    return { args, setIsLoaded, isLoaded }
+  },
+  template: `
+    <h2>  Toggle  </h2>
+    <Stack padding="4" spacing="1">
+    <Skeleton height='40px' :isLoaded="isLoaded">
+      <Box>Hello World!</Box>
+    </Skeleton>
+    <Skeleton
+      height='40px'
+      :isLoaded="isLoaded"
+      bg='green.500'
+      color='white'
+      :fadeDuration="1"
+    >
+      <Box>Hello Vue!</Box>
+    </Skeleton>
+    <Skeleton
+      height='40px'
+      :isLoaded="isLoaded"
+      :fadeDuration="1"
+      bg='blue.500'
+      color='white'
+    >
+      <Box>Hello Luniand!</Box>
+    </Skeleton>
+
+    <Box textAlign='center'>
+      <Button @click="setIsLoaded">toggle</Button>
+    </Box>
+  </Stack>
+    `,
+})
+
+export const contentIsLoaded = ToggleTemplate.bind({})
