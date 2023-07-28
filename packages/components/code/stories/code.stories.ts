e815bd8c953a66cffada1babf9d8ec808cc34681
Beae-TuanNguyen <139732634+Beae-TuanNguyen@users.noreply.github.com>
2023-07-28 11:25:32 +0700

    BSPB-98: remake storybook

--- packages/components/code/stories/code.stories.ts
+++ packages/components/code/stories/code.stories.ts
@@ -1,38 +1,41 @@
-import type { StoryFn } from "@storybook/vue3"
+import type { Meta, StoryObj } from "@storybook/vue3"
+
 import { Code } from "../src"
-export default {
-  title: "Components / Code",
-  component: {
-    Code,
+import { ref } from "vue"
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+const meta = {
+  title: "Example/Code",
+  component: Code as any,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["solid", "subtle", "outline"],
+    },
   },
+  args: {
+    variant: "subtle",
+  },
+} satisfies Meta<typeof Code>
+
+export default meta
+type Story = StoryObj<typeof meta>
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/vue/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: (args) => ({
+    components: { Code },
+    setup() {
+      return {
+        args,
+      }
+    },
+    template: `<Code :variant="args.variant">Example Code</Code>`,
+  }),
+  args: {},
 }
-
-const Template: StoryFn = (args: any) => ({
-  components: {
-    Code,
-  },
-  setup() {
-    return { args }
-  },
-  template: `
-    <Code> Hello world </Code>
-  `,
-})
-
-export const Basic = Template.bind({})
-
-const ColorCodeTemplate: StoryFn = (args: any) => ({
-  components: {
-    Code,
-  },
-  setup() {
-    return { args }
-  },
-  template: `
-    <Code>console.log(welcome)</Code>
-    <Code colorScheme='red' >var beae-ui = 'awesome!'></Code>
-    <Code colorScheme="yellow">npm install node</Code>
-  `,
-})
-
-export const ColorCode = ColorCodeTemplate.bind({})
